e09cc6d0ac95d36358b41cd4e7da34f0de36de5c
Hudson <hlaforce@potomacschool.org>
2021-10-14 08:52:22 -0400

    Changed isSpaceWater to now work

--- HumanPlayer.py
+++ HumanPlayer.py
@@ -1,4 +1,20 @@
 from Player import Player
+from Grid import Grid
 
 class HumanPlayer(Player):
-    pass
+    def __init__(self):
+        Player.__init__(self)
+
+    def placeShip(self, ship, size):
+        illegalPlacement = False
+        while(illegalPlacement):
+            shipStartX = int(input("What is the x-value of the ship's start: "))
+            shipStartY = int(input("What is the x-value of the ship's start: "))
+            direction = input("What direction is the ship facing: ")
+            if direction != size or
+
+        Grid.changeCol()
+
+
+    def takeTurn(self):
+        pass
