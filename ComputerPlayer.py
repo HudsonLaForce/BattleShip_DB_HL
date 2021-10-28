00fd71730736792b77e3b64579e2cf224e062950
DashiellBudd <dbudd@potomacschool.org>
2021-10-28 09:21:45 -0400

    worked on CP Take Turn

--- ComputerPlayer.py
+++ ComputerPlayer.py
@@ -12,37 +12,102 @@ class ComputerPlayer(Player):
         }
         Player.__init__(self)
         self.shotList = []
+        self.guessList = []
+
+
+    def isInGuessList(self,r,c):#checks if location is already in the guess list
+        for x in self.guessList:
+            if x.getRow() == r and x.getCol() == c:
+                return True
+        return False
+
+    def addToGuessList(self,r,c,s):#makes sure a guess is legal before adding it to the guess list returns tru if the guess is a hit
+        if -1 <  r < 10 and -1 <  c < 10 and self.shotGrid.returnLocation(r, c) == "~" and not self.isInGuessList(r, c):
+            if s:
+                self.guessList.insert(0,Shot("g", r, c))
+            else:
+                self.guessList.append(Shot("g", r, c))
+        elif self.shotGrid.returnLocation(r, c) == "h" :
+            return True
+
+
+
 
     def takeTurn(self, otherPlayer):
-        if self.shotList[-1].getResult() == "s":
+        for row in range(10):
+            hitCount = 0
+            for col in range(10):
+                if self.gridShots.returnLocation(row, col) == "h" :
+                    hitCount += 1
+                if self.gridShots.returnLocation(row, col) != "h" or col == 9:
+                    if hitCount >= 2:
+                        self.addToGuessList(row, col+1, True)
+                        self.addToGuessList(row, col-hitCount, True)
+                        for x in range(hitCount):
+                            self.addToGuessList(row + 1, col - x, False)
+                        for x in range(hitCount):
+                            self.addToGuessList(row - 1, col - x, False)
+                    hitCount = 0
+        for col in range(10):
+            hitCount = 0
+            for row in range(10):
+                if self.gridShots.returnLocation(row,col) == "h":
+                    hitCount += 1
+                if self.gridShots.returnLocation(row, col) != "h" or row == 9:
+                    if hitCount>=2:
+                        self.addToGuessList(row + 1,col,True)
+                        self.addToGuessList(row - hitCount, col,True)
+                        for x in range(hitCount):
+                            self.addToGuessList(row - x, col + 1, False)
+                        for x in range(hitCount):
+                            self.addToGuessList(row - x, col - 1, False)
+                        hitCount = 0
+        for row in range(10):
+            for col in range(10):
+                if self.gridShots.returnLocation(row, col) == "h":
+                    self.addToGuessList(row + 1, col, False)
+                    self.addToGuessList(row - 1, col, False)
+                    self.addToGuessList(row , col + 1 , False)
+                    self.addToGuessList(row , col - 1, False)
+        if not bool(self.guessList) :
             self.randTurn(otherPlayer)
         else:
-            pass
+            self.Turn(otherPlayer,self.guessList[0].getRow(),self.guessList[0].getCol())
 
+
+
+
+
+
+
+
+    def Turn(self, otherPlayer, r, c):
+        if otherPlayer.gridShips.returnLocation(r,c) != "~":#if guess is not water
+            print("hit")
+            self.gridShots.changeSingleSpace(r,c,"h")
+            shipHit = otherPlayer.gridShips.returnLocation(r, c)
+            otherPlayer.gridShips.changeSingleSpace(r, c, "h")
+            for row in range(10):#traverses grid
+                for col in range(10):#traverses grid
+                    if otherPlayer.gridShips.returnLocation(row,col) == shipHit:#if any spaces a left of the ship that was hit ends turn
+                        self.shipList.append(Shot(shipHit, r, c))
+                        self.updateShotList(otherPlayer,shipHit)
+                        return otherPlayer.stillHasShips()
+            print("you sunk the opponents " + self.ships[shipHit])
+            self.shipList.append(Shot("s",r,c))
+        else:#miss
+            print("miss")
+            self.gridShots.changeSingleSpace(r, c, "m")
+            otherPlayer.gridShips.changeSingleSpace(r, c, "m" )
+            self.shipList.append(Shot("m", r, c))
+        return otherPlayer.stillHasShips()
     #takes the players turn
     # otherPLayer param the opponent who's ship grid will be checked and updated
     def randTurn(self,otherPlayer):
         guess = random.randrange(0,100)
         while (not self.gridShots.isSpaceWater(guess//10,guess%10)):#makes sure guess is not one previously guessed
             guess = random.randrange(0, 100)
-        if otherPlayer.gridShips.returnLocation(guess//10,guess%10) != "~":#if guess is not water
-            print("hit")
-            self.gridShots.changeSingleSpace(guess//10,guess%10,"h")
-            shipHit = otherPlayer.gridShips.returnLocation(guess // 10, guess % 10)
-            otherPlayer.gridShips.changeSingleSpace(guess // 10, guess % 10, "h")
-            for row in range(10):#traverses grid
-                for col in range(10):#traverses grid
-                    if otherPlayer.gridShips.returnLocation(row,col) == shipHit:#if any spaces a lef of the ship that was hit ends turn
-                        self.shipList.append(Shot("h", guess // 10, guess % 10))
-                        return otherPlayer.stillHasShips()
-            print("you sunk the opponents " + self.ships[shipHit])
-            self.shipList.append(Shot("s",guess//10,guess%10))
-        else:#miss
-            print("miss")
-            self.gridShots.changeSingleSpace(guess // 10, guess % 10, "m")
-            otherPlayer.gridShips.changeSingleSpace(guess // 10, guess % 10, "m" )
-            self.shipList.append(Shot("m", guess // 10, guess % 10))
-        return otherPlayer.stillHasShips()
+        self.Turn(otherPlayer, guess//10, guess%10)
     #places a ship in the Computer Players shipGrid
     #ship param a letter representing what ship is being placed
     #size param the number of spaces that the ship takes up
@@ -75,3 +140,17 @@ class ComputerPlayer(Player):
                 if self.gridShips.returnLocation(row,col) != "~" and self.gridShips.returnLocation(row,col) != "h" and self.gridShips.returnLocation(row,col) != "m":#if a ship is found returns true
                     return True
         return False
+
+    def updateShotList(self,otherPlayer,ship):
+        for row in range(10):
+            for col in range(10):
+                if otherPlayer.gridShips.returnLocation(row,col) == ship:
+                    for x in range(len(self.shotList)):
+                        if row == x.getRow() and col == x.getCol():
+                            self.shotList.pop(x)
+        for y in range(len(self.shotList)):
+            if self.shotList[y] == "h":
+                return
+        self.shotList == []
+        self.guessList == []
+
